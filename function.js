254e477e05b75d2ef4452836507134dfb35807f0
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-01-29 08:06:16 +0600

    basic recap

---
+++ function.js
@@ -0,0 +1,30 @@
+var fovoriteBook = "4 hour wrk week";
+var BookList = ["positionning", "hooked", "start with why"];
+var shoeDogIndex = BookList.indexOf("hooked");
+BookList[1] = "dont hooked";
+BookList.push("small giants");
+BookList.pop();
+
+// conditionals
+if (BookList[1] == "hooked"){
+    console.log("iam hooked");
+}
+
+else {
+    console.log("i am not hooked");
+
+}
+
+// while loop 
+var i = 0;
+while(i < 15){
+    console.log(i);
+    i++;
+}
+
+// for Loop
+for (var i=0; i <15; i++){
+    console.log(i);
+}
+
+// function
