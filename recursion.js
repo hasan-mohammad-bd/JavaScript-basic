9ff6bb212eee9fb8cf8966d65a71c0165bdb32b5
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-06 11:54:17 +0600

    adding recursion

---
+++ recursion.js
@@ -0,0 +1,15 @@
+
+//recursion
+
+function  consoleNumber(i){
+    if(i > 5){
+        return;
+    }
+    console.log(i);
+    consoleNumber(i+1);
+}
+
+consoleNumber(1)
+
+//++i vs i++ , find the different between them from google
+
